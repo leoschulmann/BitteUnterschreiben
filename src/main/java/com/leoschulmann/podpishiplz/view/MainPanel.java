50a82b1aa41c149e9df159c87ec104792f1e9cc6
levchigintsev <lev.chigintsev@gmail.com>
2020-08-07 00:19:53 +0300

    Overlays now clickable; scaling fixed

--- src/main/java/com/leoschulmann/podpishiplz/view/MainPanel.java
+++ src/main/java/com/leoschulmann/podpishiplz/view/MainPanel.java
@@ -3,23 +3,39 @@ package com.leoschulmann.podpishiplz.view;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainPanel extends JPanel {
-    private static java.util.List<BufferedImage> overlays = new ArrayList<>();   //todo Map<Image, ImageCoords>
+    private static final java.util.List<Overlay> overlays = new ArrayList<>();
     private BufferedImage bufIm;
     private int resizeHeight = 0;
     private int resizeWidth = 0;
     private double resizeRatio = 0.0;
     private static final int INSET = 10;
     private int x1 = 0;
-    private int x2 = 0;
     private int y1 = 0;
-    private int y2 = 0;
 
+    public MainPanel() {
+//        MouseController mouse = new MouseController(overlays);
+//        addMouseListener(mouse);
+//        addMouseMotionListener(mouse);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                overlays.forEach(overlay -> overlay.setSelected(false));
+                overlays.stream().filter(o -> o.getBounds().contains(e.getX(), e.getY()))
+                        .findFirst()
+                        .ifPresent(o -> o.setSelected(true));
+                repaint();
+            }
+        });
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -29,31 +45,38 @@ public class MainPanel extends JPanel {
                 resizeWidth = resizeHeight * bufIm.getWidth() / bufIm.getHeight();
                 x1 = (this.getWidth() - resizeWidth) / 2;
                 y1 = INSET;
-                x2 = x1 + resizeWidth;
-                y2 = y1 + resizeHeight;
 
             } else { // horizontal
                 resizeWidth = this.getWidth() - (INSET * 2);
                 resizeHeight = resizeWidth * bufIm.getHeight() / bufIm.getWidth();
-
                 x1 = INSET;
                 y1 = (this.getHeight() - resizeHeight) / 2;
-                x2 = x1 + resizeWidth;
-                y2 = y1 + resizeHeight;
             }
 
             g.drawImage(bufIm, x1, y1, resizeWidth, resizeHeight, null);
 
             resizeRatio = 1.0 * resizeHeight / bufIm.getHeight();
-//            System.out.println("Current scale:\t" + resizeRatio);
-//            System.out.println("Page coords:\t[" + x1 + "," + y1 + "]\t[" + x2 + "," + y2 + "]");
         }
+
         if (overlays.size() > 0) {
-            for (BufferedImage i : overlays) {
-                g.drawImage(i, (x1 + x2) / 2, (y1 + y2) / 2,
-                        (int) (i.getWidth() * resizeRatio), (int) (i.getHeight() * resizeRatio), null);
-            }
+            overlays.forEach(o -> {
+                int overlayResizeWidth = (int) (o.getWidth() * resizeRatio);
+                int overlayResizeHeight = (int) (o.getHeight() * resizeRatio);
+                int overlayX = x1 + (int) (o.getRelCentX() * resizeWidth) - overlayResizeWidth / 2;
+                int overlayY = y1 + (int) (o.getRelCentY() * resizeHeight) - overlayResizeHeight / 2;
+                o.setBounds(overlayX, overlayY, overlayResizeWidth, overlayResizeHeight);
+                g.drawImage(o.getImage(), overlayX, overlayY, overlayResizeWidth, overlayResizeHeight, null);
+            });
         }
+
+        overlays.stream().filter(Overlay::isSelected)
+                .findFirst()
+                .ifPresent(o -> {
+                    Graphics2D g2 = (Graphics2D) g;
+                    g2.setColor(Color.GREEN);
+                    g2.setStroke(new BasicStroke(3));
+                    g.drawRect(o.getBounds().x, o.getBounds().y, o.getBounds().width, o.getBounds().height);
+                });
     }
 
     public void setBufferedImage(BufferedImage bufferedImage) {
@@ -61,7 +84,11 @@ public class MainPanel extends JPanel {
     }
 
     public void addNewOverlay(String file) throws IOException {
-        BufferedImage overlay = ImageIO.read(new File(file));
-        overlays.add(overlay);
+        Overlay o = new Overlay(ImageIO.read(new File(file)));
+        o.setRelCentX(Math.random());
+        o.setRelCentY(Math.random());
+        o.setSelected(true);
+        overlays.forEach(overlay -> overlay.setSelected(false));
+        overlays.add(o);
     }
 }
---
+++ src/main/java/com/leoschulmann/podpishiplz/view/Overlay.java
@@ -0,0 +1,62 @@
+package com.leoschulmann.podpishiplz.view;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Overlay {
+    private final BufferedImage image;
+    private boolean selected;
+    private double relCentX;
+    private double relCentY;
+    private final Rectangle bounds;
+
+
+    public Overlay(BufferedImage image) {
+        this.image = image;
+        this.bounds = new Rectangle();
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public int getWidth() {
+        return image.getWidth();
+    }
+
+    public int getHeight() {
+        return image.getHeight();
+    }
+
+    public double getRelCentX() {
+        return relCentX;
+    }
+
+    public void setRelCentX(double relCentX) {
+        this.relCentX = relCentX;
+    }
+
+    public double getRelCentY() {
+        return relCentY;
+    }
+
+    public void setRelCentY(double relCentY) {
+        this.relCentY = relCentY;
+    }
+
+    public void setBounds(int x, int y, int width, int height) {
+        this.bounds.setBounds(x, y, width, height);
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
+}
