ce832cc1a0ebcc3d7cabce0f235da0f75e244b54
levchigintsev <lev.chigintsev@gmail.com>
2020-09-02 17:30:09 +0300

    mockito tests

--- src/test/java/com/leoschulmann/podpishiplz/controller/DocumentControllerTest.java
+++ src/test/java/com/leoschulmann/podpishiplz/controller/DocumentControllerTest.java
@@ -1,9 +1,14 @@
 package com.leoschulmann.podpishiplz.controller;
 
 import com.leoschulmann.podpishiplz.model.Page;
+import com.leoschulmann.podpishiplz.view.MainPanel;
+import com.leoschulmann.podpishiplz.view.TopScrollerPanel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.swing.*;
 
 class DocumentControllerTest {
 
@@ -78,4 +83,53 @@ class DocumentControllerTest {
         Assertions.assertThrows(IllegalArgumentException.class, ()-> DocumentController.setCurrentPage(page));
     }
 
+    @Test
+    void openAndDeleteLast() {
+        Page p1 = new Page("", 0);
+        Page p2 = new Page("", 0);
+        Page p3 = new Page("", 0);
+        DocumentController.addPage(p1);
+        DocumentController.addPage(p2);
+        DocumentController.addPage(p3);
+        DocumentController.setCurrentPage(p3);
+        MainPanel mainPanelMock = Mockito.mock(MainPanel.class);
+        Mockito.doNothing().when(mainPanelMock).repaint();
+        MainPanelController.setMainPanel(mainPanelMock);
+        DocumentController.deletePage(p3);
+        Assertions.assertEquals(p2, DocumentController.getCurrentPage());
+    }
+
+    @Test
+    void openAndDeleteMiddle() {
+        Page p1 = new Page("", 0);
+        Page p2 = new Page("", 0);
+        Page p3 = new Page("", 0);
+        DocumentController.addPage(p1);
+        DocumentController.addPage(p2);
+        DocumentController.addPage(p3);
+        DocumentController.setCurrentPage(p2);
+        MainPanel mainPanelMock = Mockito.mock(MainPanel.class);
+        Mockito.doNothing().when(mainPanelMock).repaint();
+        MainPanelController.setMainPanel(mainPanelMock);
+        DocumentController.deletePage(p2);
+        Assertions.assertEquals(p3, DocumentController.getCurrentPage());
+    }
+
+    @Test
+    void openAndDeleteSingle() {
+        MainPanel mainPanelMock = Mockito.mock(MainPanel.class);
+        Mockito.doNothing().when(mainPanelMock).repaint();
+        MainPanelController.setMainPanel(mainPanelMock);
+        GUIController.initListener();
+        JPanel panelmock = Mockito.mock(JPanel.class);
+        Mockito.doNothing().when(panelmock).removeAll();
+        TopScrollerPanel tspmock = Mockito.mock(TopScrollerPanel.class);
+        Mockito.when(tspmock.getPanel()).thenReturn(panelmock);
+        TopPanelController.setTsp(tspmock);
+        Page p = new Page("", 0);
+        DocumentController.addPage(p);
+        DocumentController.setCurrentPage(p);
+        DocumentController.deletePage(p);
+        Assertions.assertNull(DocumentController.getCurrentPage());
+    }
 }
