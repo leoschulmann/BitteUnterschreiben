c3ca0bee3eed6cb863fdea36a4b67bf027607de4
levchigintsev <lev.chigintsev@gmail.com>
2020-09-09 23:08:03 +0300

    '-v' parameter enables console output on DEBUG and INFO

--- src/main/java/com/leoschulmann/podpishiplz/BitteUnterschreiben.java
+++ src/main/java/com/leoschulmann/podpishiplz/BitteUnterschreiben.java
@@ -1,7 +1,12 @@
 package com.leoschulmann.podpishiplz;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
 import com.leoschulmann.podpishiplz.controller.*;
 import com.leoschulmann.podpishiplz.view.AppWindow;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.io.IOException;
@@ -10,7 +15,20 @@ import java.lang.reflect.InvocationTargetException;
 public class BitteUnterschreiben {
     static AppWindow app;
 
-    public static void main(String[] asda) throws InvocationTargetException, InterruptedException {
+    public static void main(String[] args) throws InvocationTargetException, InterruptedException {
+
+        //default behavior: only write errors to err.log
+        Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        Appender<ILoggingEvent> consoleLogger = root.getAppender("STDOUT");
+        root.detachAppender(consoleLogger);
+        root.setLevel(Level.WARN);
+
+        //'-v' argument enables 'verbose' and writes all debug and info messages to console
+        if (args.length > 0 && args[0].equals("-v")) {
+            root.setLevel(Level.DEBUG);
+            root.addAppender(consoleLogger);
+            LoggerFactory.getLogger(BitteUnterschreiben.class).info("Console logging enabled (-v)");
+        }
         SwingUtilities.invokeAndWait(() -> app = new AppWindow());
         init();
     }
