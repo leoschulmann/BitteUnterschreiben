d3926f23e12a06e03c044da6c28529c9c7ca381c
levchigintsev <lev.chigintsev@gmail.com>
2020-09-10 23:59:21 +0300

    SettingsDialogue rework

--- src/main/java/com/leoschulmann/podpishiplz/view/SettingsDialogue.java
+++ src/main/java/com/leoschulmann/podpishiplz/view/SettingsDialogue.java
@@ -1,8 +1,10 @@
 package com.leoschulmann.podpishiplz.view;
 
 import com.leoschulmann.podpishiplz.controller.SettingsController;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import java.io.IOException;
 
@@ -16,6 +18,10 @@ public class SettingsDialogue extends JDialog {
     private int blendingMode;
     private float jpgQuality;
     private float resolution;
+    private static ImageIcon noBlendingIcon;
+    private static ImageIcon darkenBlendingIcon;
+    private static ImageIcon multiplyBlendingIcon;
+
 
     public SettingsDialogue(JFrame appWindow, int blendingMode, float jpgQuality, float resolution) {
         this.blendingMode = blendingMode;
@@ -34,7 +40,7 @@ public class SettingsDialogue extends JDialog {
         JButton ok = new JButton("OK");
         ok.addActionListener(e -> confirm());
         getRootPane().setDefaultButton(ok);
-        JButton cancel = new JButton("cancel");
+        JButton cancel = new JButton("Отмена");
         cancel.addActionListener(e -> setVisible(false));
         jpegQltySlider = new JSlider(JSlider.HORIZONTAL, 0, 100, (int) (jpgQuality * 100));
         jpegQltySlider.setMajorTickSpacing(20);
@@ -45,58 +51,60 @@ public class SettingsDialogue extends JDialog {
 
         dpiSlider = new JSlider(JSlider.HORIZONTAL, 100, 600, (int) (resolution * 300));
         dpiSlider.setMajorTickSpacing(100);
+        dpiSlider.setMinorTickSpacing(50);
         dpiSlider.setSnapToTicks(true);
         dpiSlider.setPaintTicks(true);
         dpiSlider.setPaintLabels(true);
 
-        //placeholder
-        ImageIcon noBlendingIcon = new ImageIcon(appWindow.getClass().getClassLoader().getResource("no_blending.png"));
-        ImageIcon darkenBlendingIcon = new ImageIcon(appWindow.getClass().getClassLoader().getResource("darken_blending.png"));
-        ImageIcon multiplyBlendingIcon = new ImageIcon(appWindow.getClass().getClassLoader().getResource("multiply_blending.png"));
+        //images
+        noBlendingIcon = new ImageIcon(appWindow.getClass().getClassLoader().getResource("no_blending.png"));
+        darkenBlendingIcon = new ImageIcon(appWindow.getClass().getClassLoader().getResource("darken_blending.png"));
+        multiplyBlendingIcon = new ImageIcon(appWindow.getClass().getClassLoader().getResource("multiply_blending.png"));
+
+        //dummy
+        JPanel dummy = new JPanel();
+        dummy.setPreferredSize(new Dimension(40, 10));
 
         //draw components
+        gbc.anchor = GridBagConstraints.LINE_START;
         gbc.gridx = 0;
         gbc.gridy = 0;
-
-        add(wrapPanel(new JLabel(null, noBlendingIcon, JLabel.CENTER)), gbc); //todo implement clickable
-
-        gbc.gridx++;
-        add(wrapPanel(new JLabel(null, darkenBlendingIcon, JLabel.CENTER)), gbc);
-
-        gbc.gridx++;
-        add(wrapPanel(new JLabel(null, multiplyBlendingIcon, JLabel.CENTER)), gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy++;
-        add(noopBlender, gbc);
-
-        gbc.gridx++;
-        add(darkenBlender, gbc);
-
-        gbc.gridx++;
-        add(multiplyBlender, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy++;
-        gbc.gridwidth = 1;
-        add(new JLabel("Степень сжатия JPEG"), gbc);
-        gbc.gridx++;
-        add(jpegQltySlider, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy++;
-        gbc.gridwidth = 1;
-        add(new JLabel("Разрешение изображений, dpi"), gbc);
-        gbc.gridx++;
-        add(dpiSlider, gbc);
+        add(wrapPanel(true, drawBlendingRadio()), gbc);
 
         gbc.gridy++;
-        gbc.gridx = 0;
-        gbc.gridwidth = 3;
+        add(wrapPanel(true, new JLabel("Степень сжатия JPEG"), dummy, jpegQltySlider), gbc);
+
+        gbc.gridy++;
+        add(wrapPanel(true, new JLabel("Разрешение изображений, dpi"), dpiSlider), gbc);
+
+        gbc.gridy++;
         gbc.anchor = GridBagConstraints.LINE_END;
-        add(wrapPanel(ok, cancel), gbc);
+        add(wrapPanel(false, ok, cancel), gbc);
+
         pack();
         setLocationRelativeTo(appWindow);
+    }
+
+    static private JPanel drawBlendingRadio() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 20));
+        JPanel noBlendingPanel = new JPanel(new BorderLayout());
+        noBlendingPanel.add(new JLabel(null, SettingsDialogue.noBlendingIcon, JLabel.CENTER), BorderLayout.CENTER);
+        noBlendingPanel.add(noopBlender, BorderLayout.SOUTH);
+
+        JPanel darkenBlendingPanel = new JPanel(new BorderLayout());
+        darkenBlendingPanel.add(new JLabel(null, SettingsDialogue.darkenBlendingIcon, JLabel.CENTER), BorderLayout.CENTER);
+        darkenBlendingPanel.add(darkenBlender, BorderLayout.SOUTH);
+
+        JPanel multiplyBlendingPanel = new JPanel(new BorderLayout());
+        multiplyBlendingPanel.add(new JLabel(null, SettingsDialogue.multiplyBlendingIcon, JLabel.CENTER), BorderLayout.CENTER);
+        multiplyBlendingPanel.add(multiplyBlender, BorderLayout.SOUTH);
+
+        panel.add(noBlendingPanel);
+        panel.add(darkenBlendingPanel);
+        panel.add(multiplyBlendingPanel);
+
+        return panel;
     }
 
     public void setBlendingMode(int blendingMode) {
@@ -111,17 +119,17 @@ public class SettingsDialogue extends JDialog {
         this.resolution = resolution;
     }
 
-    private JPanel wrapPanel(JButton ok, JButton cancel) {
-        JPanel jp = new JPanel();
-        jp.add(ok);
-        jp.add(cancel);
-        return jp;
-    }
+    private JPanel wrapPanel(boolean bordered, Component... components) {
+        JPanel jp = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        for (Component c : components) {
+            jp.add(c);
+        }
+        if (bordered) jp.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
 
-    private JPanel wrapPanel(JLabel jLabel) {
-        JPanel jp = new JPanel();
-        jp.add(jLabel);
-        return jp;
+        JPanel outer = new JPanel();
+        outer.setBorder(BorderFactory.createEmptyBorder(3, 0, 3, 0));
+        outer.add(jp);
+        return outer;
     }
 
     private void confirm() {
@@ -138,15 +146,15 @@ public class SettingsDialogue extends JDialog {
             SettingsController.saveYML();
         } catch (IOException e) {
             JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-            e.printStackTrace();
+            LoggerFactory.getLogger(SettingsDialogue.class).error(e.getMessage(), e);
         }
     }
 
     private ButtonGroup initRadios(int i) {
         ButtonGroup buttonGroup = new ButtonGroup();
-        noopBlender = new JRadioButton("Без смешивания");
-        darkenBlender = new JRadioButton("Смешивание с затемнением");
-        multiplyBlender = new JRadioButton("Смешивание с умножением");
+        noopBlender = new JRadioButton("Без смешения");
+        darkenBlender = new JRadioButton("Смешение с затемнением");
+        multiplyBlender = new JRadioButton("Смешение с умножением");
         btns = new JRadioButton[]{noopBlender, darkenBlender, multiplyBlender};
         buttonGroup.add(noopBlender);
         buttonGroup.add(darkenBlender);
